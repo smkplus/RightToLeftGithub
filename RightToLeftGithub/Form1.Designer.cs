1595be9dc0c1c3bd8bace0d15637e9b44a755e16
smkplus <smkplus3d@gmail.com>
2018-12-31 12:34:09 +0330

    second commit

---
+++ RightToLeftGithub/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace RightToLeftGithub
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input = new System.Windows.Forms.TextBox();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.Convert = new System.Windows.Forms.Button();
+            this.TypeBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(12, 58);
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(311, 269);
+            this.Input.TabIndex = 0;
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(329, 58);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(311, 269);
+            this.Output.TabIndex = 1;
+            // 
+            // Convert
+            // 
+            this.Convert.Location = new System.Drawing.Point(257, 372);
+            this.Convert.Name = "Convert";
+            this.Convert.Size = new System.Drawing.Size(150, 30);
+            this.Convert.TabIndex = 2;
+            this.Convert.Text = "Convert";
+            this.Convert.UseVisualStyleBackColor = true;
+            this.Convert.Click += new System.EventHandler(this.Convert_Click);
+            // 
+            // TypeBox
+            // 
+            this.TypeBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TypeBox.FormattingEnabled = true;
+            this.TypeBox.Items.AddRange(new object[] {
+            "Normal",
+            "Bold",
+            "Italic",
+            "H1",
+            "H2",
+            "H3",
+            "H4",
+            "H5"});
+            this.TypeBox.Location = new System.Drawing.Point(274, 342);
+            this.TypeBox.Name = "TypeBox";
+            this.TypeBox.Size = new System.Drawing.Size(121, 24);
+            this.TypeBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(465, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "HTML";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 454);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TypeBox);
+            this.Controls.Add(this.Convert);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "RightToLeftGithub";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Button Convert;
+        private System.Windows.Forms.ComboBox TypeBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RightToLeftGithub/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RightToLeftGithub
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TypeBox.SelectedIndex = 0;
+            Input.Text = "WriteHere";
+            Output.Text = "ConvertedText";
+
+
+        }
+
+        private void Convert_Click(object sender, EventArgs e)
+        {
+            string pattern = @"(.+)";
+            string substitution = "";
+            var Type = TypeBox.SelectedItem.ToString();
+            if (Type == "Normal")
+            {
+                substitution = @"<div dir=""rtl""> $0 </div> ";
+            }
+            else if (Type == "Bold")
+            {
+                substitution = @"<div dir=""rtl""> <b> $0 </b> </div> ";
+            }
+            else if (Type == "Italic")
+            {
+                substitution = @"<div dir=""rtl""> <i> $0 </i> </div> ";
+            }
+            else if (Type == "H1")
+            {
+                substitution = @"<div dir=""rtl""> <h1> $0 </h1> </div> ";
+            }
+            else if (Type == "H2")
+            {
+                substitution = @"<div dir=""rtl""> <h2> $0 </h2> </div> ";
+            }
+            else if (Type == "H3")
+            {
+                substitution = @"<div dir=""rtl""> <h3> $0 </h3> </div> ";
+            }
+            else if (Type == "H4")
+            {
+                substitution = @"<div dir=""rtl""> <h4> $0 </h4> </div> ";
+            }
+            else if (Type == "H5")
+            {
+                substitution = @"<div dir=""rtl""> <h5> $0 </h5> </div> ";
+            }
+            if(substitution != "")
+            {
+
+            Regex regex = new Regex(pattern);
+            Output.Text = regex.Replace(Input.Text, substitution);
+            Clipboard.SetText(Output.Text);
+            MessageBox.Show("Text Converted And Copied");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
